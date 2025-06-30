8609b457a939a89f88bed2b1ceec1a6fcd77749b
Willian Prado <154278185+willianprado23@users.noreply.github.com>
2025-06-30 20:30:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let nac = 85;
 let badBunny = 25;
 const maluma = badBunny * nac;
 console.log(maluma);
+
+
+let mes = "Junho";
+console.log(`Meu mes favorito é ${mes}`);
+
+const estaTocandoMusica = true;
+console.log(estaTocandoMusica);
